b2c9b3e5fa5eb4453719387fbc97b3aa1ea88962
Kiran_Kumar_Gangadharaiah <kirancg93@gmail.com>
2020-05-14 15:36:40 -0400

    Added all the setup files and App file

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Component } from 'react';
+
+//initialize component
+
+class App extends React.Component {
+  
+  //built react method returns JSX(JS with HTML)(Babel converts it into JavaScript)
+  render() {
+    return (
+
+    <div>Hello</div>
+    //Can return only one parent element
+    //Use Fragment for more div
+    )
+  }
+}
+
+export default App;
